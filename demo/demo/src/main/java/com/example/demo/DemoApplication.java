83b616154d7af392ab2bc6e0edf830de98cd9ff1
ramos-vini <ramoscontrole@gmail.com>
2023-07-17 21:47:52 +0200

    Student Class

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.students.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,10 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student("Johnny", "johnnybravo@email.com", LocalDate.of(2000,7, 24))
+		);
 	}
 
 }
---
+++ demo/demo/src/main/java/com/example/demo/students/Student.java
@@ -0,0 +1,58 @@
+package com.example.demo.students;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private Long id;
+    private String email;
+    private LocalDate birth;
+
+    public Student(String name, String email, LocalDate birth) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", email='" + email + '\'' +
+                ", birth=" + birth +
+                '}';
+    }
+}
